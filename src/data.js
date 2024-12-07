63629c39bb2e44813feb26edf124f1418297d7a5
soumit-s <soumit.srim@gmail.com>
2024-12-07 20:28:51 +0530

    Added rendering of the person graph

--- src/data.js
+++ src/data.js
@@ -5,34 +5,12 @@
 // geenrate connection between them
 // 0 = no connection
 // single dimension ith row jth column = i*n+j, size = n*n
+import p5 from "p5";
 
-class Node {
+export class Node {
   constructor(id, x, y, entropy) {
     this.id = id;
     this.position = { x, y };
     this.entropy = entropy;
   }
-}
-
-function generateNodes(n) {
-  let nodes = [];
-  for (let i = 0; i < n; i++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    let entropy = random(0, 1); // entropy value between 0 and 1
-    nodes.push(new Node(i, x, y, entropy));
-  }
-  return nodes;
-}
-
-function generateAdjacencyMatrix(n, maxFriendliness) {
-  let matrix = new Array(n * n).fill(0); // Flat array
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      let friendliness = floor(random(0, maxFriendliness + 1));
-      matrix[i * n + j] = friendliness;
-      matrix[j * n + i] = friendliness;
-    }
-  }
-  return matrix;
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,42 @@
-import p5 from "p5";
+import { Node } from "./data";
 
 export const sketch = (p) => {
   let acc = 0;
   const timestep = 1000; // In milliseconds
+
+  let numPeople = 50;
+  let maxFriendliness = 10;
 
   let state = {
     nodes: [],
     adjacencyMatrix: [],
   };
 
+  function generateNodes(n) {
+    let nodes = [];
+    for (let i = 0; i < n; i++) {
+      let x = p.random(0, p.width);
+      let y = p.random(0, p.height);
+      let entropy = p.random(0, 1); // entropy value between 0 and 1
+      nodes.push(new Node(i, x, y, entropy));
+    }
+    return nodes;
+  }
+  
+  function generateAdjacencyMatrix(n, maxFriendliness) {
+    let matrix = new Array(n * n).fill(0); // Flat array
+    for (let i = 0; i < n; i++) {
+      for (let j = i + 1; j < n; j++) {
+        let friendliness = p.floor(p.random(0, maxFriendliness + 1));
+        matrix[i * n + j] = friendliness;
+        matrix[j * n + i] = friendliness;
+      }
+    }
+    return matrix;
+  }
+
   const simulate = (fixedTimestep) => {
-    console.log("Simulating after", fixedTimestep);
+    // console.log("Simulating after", fixedTimestep);
   };
 
   p.setup = () => {
@@ -18,9 +44,9 @@ export const sketch = (p) => {
     window.onresize = () =>
       p.resizeCanvas(window.innerWidth, window.innerHeight);
 
-    state.nodes = generateNodes(numFriends);
+    state.nodes = generateNodes(numPeople);
     state.adjacencyMatrix = generateAdjacencyMatrix(
-      numFriends,
+      numPeople,
       maxFriendliness
     );
   };
@@ -33,7 +59,23 @@ export const sketch = (p) => {
     }
 
     p.background(200);
-    p.fill(100, 150, 255);
-    p.ellipse(p.mouseX, p.mouseY, 50, 50);
+
+    p.stroke(150, 150, 150);
+    for (let i = 0; i < numPeople; ++i) {
+      for (let j = 0; j < numPeople; ++j) {
+        if (i == j) continue;
+        if (state.adjacencyMatrix[i * numPeople + j] > 0) {
+          const inode = state.nodes[i];
+          const jnode = state.nodes[j];
+          p.line(inode.position.x, inode.position.y, jnode.position.x, jnode.position.y);
+        }
+      }
+    }
+
+    p.fill(70, 70, 70);
+    p.stroke(70, 70, 70);
+    for (const node of state.nodes) {
+      p.circle(node.position.x, node.position.y, 6);
+    }
   };
 };
