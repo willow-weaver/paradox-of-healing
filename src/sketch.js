9c8480a7a0207c34d4e233006e0e8fbb3648dcc4
soumit-s <soumit.srim@gmail.com>
2024-12-07 19:29:53 +0530

    Added fixed timestep bound simulate function

--- src/sketch.js
+++ src/sketch.js
@@ -1,11 +1,25 @@
 import p5 from "p5";
 
 export const sketch = (p) => {
+  let acc = 0;
+  const timestep = 1000; // In milliseconds
+
+  const simulate = (fixedTimestep) => {
+    console.log("Simulating after", fixedTimestep);
+  };
+
   p.setup = () => {
-    p.createCanvas(400, 400);
+    p.createCanvas(window.innerWidth, window.innerHeight);
+    window.onresize = () => p.resizeCanvas(window.innerWidth, window.innerHeight);
   };
 
   p.draw = () => {
+    acc += p.deltaTime;
+    if (acc > timestep) {
+      acc -= timestep;
+      simulate(timestep);
+    }
+
     p.background(200);
     p.fill(100, 150, 255);
     p.ellipse(p.mouseX, p.mouseY, 50, 50);
