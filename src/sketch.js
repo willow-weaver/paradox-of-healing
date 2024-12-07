3e55831c13a043430e9f3ac1af769a94a18db491
nabameet <thenabameet@gmail.com>
2024-12-07 20:00:19 +0530

    added state

--- src/sketch.js
+++ src/sketch.js
@@ -4,13 +4,25 @@ export const sketch = (p) => {
   let acc = 0;
   const timestep = 1000; // In milliseconds
 
+  let state = {
+    nodes: [],
+    adjacencyMatrix: [],
+  };
+
   const simulate = (fixedTimestep) => {
     console.log("Simulating after", fixedTimestep);
   };
 
   p.setup = () => {
     p.createCanvas(window.innerWidth, window.innerHeight);
-    window.onresize = () => p.resizeCanvas(window.innerWidth, window.innerHeight);
+    window.onresize = () =>
+      p.resizeCanvas(window.innerWidth, window.innerHeight);
+
+    state.nodes = generateNodes(numFriends);
+    state.adjacencyMatrix = generateAdjacencyMatrix(
+      numFriends,
+      maxFriendliness
+    );
   };
 
   p.draw = () => {
